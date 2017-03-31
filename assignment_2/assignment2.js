78a894200483f52d61873d18aa81b930b7085e0c
dbchung2 <dbchung2@gamil.com>
2017-03-31 12:47:44 -0400

    first and second finished

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -3,6 +3,7 @@ Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
 
+for(let i = 1 ; i <= 100 ; i++) console.log(i);
 
 
 /*
@@ -17,3 +18,9 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+
+for(var i = 1 ; i < 8 ; i ++) {
+    let temp = '';
+    for(var j = i ; j > 0 ; j--) temp = temp + "*";
+    console.log(temp);
+}
